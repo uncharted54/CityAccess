3082b50d909708c9c0cc83f20bef2268da43664f
Valentin-L <vlaure54@gmail.com>
2015-04-02 15:55:51 +0200

    Plein de trucs

--- Includes/fonction.php
+++ Includes/fonction.php
@@ -2,7 +2,7 @@
 
 
 function appartient($ville){
-    $all=array('Paris','Nancy','Metz');
+    $all=array('Paris','Nancy','Metz','Laxou','Metz');
     $val=false;
     for($i=0;$i<count($all)-1;$i++){
         if ($all[$i]==$ville) {
@@ -19,9 +19,10 @@ function carte_ville($query){
 
 
 $query = $_POST["ville"];
-$query2 = $query;
 
 $url = 'http://fr.wikipedia.org/w/api.php?format=json&action=parse&page='.$query;
+
+if (appartient($query)) {
 
 $ch = curl_init($url);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -67,7 +68,8 @@ while ($e==0) {
 $text="";
 $e=0;
 
-while ($e==0) {
+
+    while ($e==0) {
 
     if (($content[$i].$content[$i+1].$content[$i+2])=="yme") {
         $e=1;
@@ -83,6 +85,28 @@ $text[1]="";
 $text[2]="";
 
 $text=$text.'my"></span></div></div></div></div></div></td></tr></table>';
+
+   
+
+}else{
+
+$ch = curl_init($url);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($ch, CURLOPT_USERAGENT, "TestScript");
+$c = curl_exec($ch);
+
+$json = json_decode($c);
+
+$content = $json->{'parse'}->{'text'}->{'*'}; 
+
+$pattern = '#<p>(.*)</p>#Us'; 
+if(preg_match($pattern, $content, $matches))
+{
+    $text= strip_tags($matches[1]);
+}
+
+}
+
 
 return $text;
 }
--- page0/index.php
+++ page0/index.php
@@ -102,11 +102,10 @@ echo Tableau();
 
 
 // DEBUT TRUC WIKIPEDIA
-$thisville = $_POST["ville"];
 
-if (appartient($thisville)) {
-	echo carte_ville($thisville);
-}
+$thisville = $_POST["ville"];
+echo carte_ville($thisville);
+
 // FIN TRUC 'de psychopathe' AVEC WIKIPEDIA 
 
 
@@ -114,7 +113,7 @@ if (appartient($thisville)) {
 
 
 
-<?php echo '<iframe style="margin: 10px"src= "http://www.panoramio.com/wapi/template/list.html?tag='.$query.'&amp;width=500&amp;height=80&amp;rows=2&amp;columns=20&amp;orientation=horizontal" frameborder="0" width="500" height="80" scrolling="yes" marginwidth="0" marginheight="0"> </iframe>'?>
+<?php echo '<iframe style="margin: 10px"src= "http://www.panoramio.com/wapi/template/list.html?tag='.$_POST["ville"].'&amp;width=600&amp;height=120&amp;rows=1&amp;columns=8&amp;orientation=horizontal" frameborder="0" width="600" height="120" scrolling="yes" marginwidth="0" marginheight="0"> </iframe>'?>
 
 
 
